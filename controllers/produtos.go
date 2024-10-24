7e159486541ecfb4f56bdd1bc0c03483a5b3676c
vicentemourao <mouraovicente1@gmail.com>
2024-10-24 02:44:18 -0300

    Update

--- controllers/produtos.go
+++ controllers/produtos.go
@@ -50,3 +50,38 @@ func Delete(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, "/", 301)
 
 }
+
+func Edit(w http.ResponseWriter, r *http.Request) {
+	idProduct := r.URL.Query().Get("id")
+	product := models.SelectById(idProduct)
+	tamp.ExecuteTemplate(w, "Edit", product)
+
+}
+
+func Update(w http.ResponseWriter, r *http.Request) {
+
+	if r.Method == "POST" {
+		id := r.FormValue("id")
+		nome := r.FormValue("nome")
+		descricao := r.FormValue("descricao")
+		preco := r.FormValue("preco")
+		quantidade := r.FormValue("quantidade")
+
+		precoConvertido, err := strconv.ParseFloat(preco, 64)
+		if err != nil {
+			log.Println("Erro ao converter Preco")
+		}
+
+		quantidadeConvertida, err := strconv.Atoi(quantidade)
+		if err != nil {
+			log.Println("Erro ao converter quantidade")
+		}
+		idConvertido, err := strconv.Atoi(id)
+		if err != nil {
+			log.Println("Erro ao converter quantidade")
+		}
+		models.Update(idConvertido, nome, descricao, precoConvertido, quantidadeConvertida)
+	}
+	http.Redirect(w, r, "/", 301)
+
+}
--- models/produtos.go
+++ models/produtos.go
@@ -62,3 +62,42 @@ func DeleteProduct(id string) {
 	defer db.Close()
 
 }
+
+func SelectById(id string) Produto {
+	db := db.ConnectDB()
+	product, err := db.Query("select * from produtos where id = $1", id)
+	if err != nil {
+		panic(err.Error())
+	}
+	p := Produto{}
+	for product.Next() {
+		var id, quantidade int
+		var nome, descricao string
+		var preco float64
+
+		err = product.Scan(&id, &nome, &descricao, &preco, &quantidade)
+		if err != nil {
+			panic(err.Error())
+		}
+		p.Id = id
+		p.Nome = nome
+		p.Descricao = descricao
+		p.Preco = preco
+		p.Quantidade = quantidade
+	}
+
+	defer db.Close()
+	return p
+}
+
+func Update(id int, nome, descricao string, preco float64, quantidade int) {
+	db := db.ConnectDB()
+	updateProduct, err := db.Prepare("update produtos set nome=$1, descricao=$2, preco=$3, quantidade=$4 where id=$5")
+
+	if err != nil {
+		panic(err.Error())
+	}
+	updateProduct.Exec(nome, descricao, preco, quantidade, id)
+	defer db.Close()
+
+}
